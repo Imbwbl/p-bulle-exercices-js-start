51f55b0c09e8b248c90873debc3722c2813ccbc3
bwbl <theophile2006@gmail.com>
2025-11-07 15:37:05 +0100

    WIP elyses-enchantments

--- exercices/elyses-enchantments/enchantments.js
+++ exercices/elyses-enchantments/enchantments.js
@@ -9,7 +9,7 @@
  * @returns {number} the card
  */
 export function getItem(cards, position) {
-  throw new Error('Implement the getItem function');
+  return cards[position];
 }
 
 /**
@@ -22,7 +22,8 @@ export function getItem(cards, position) {
  * @returns {number[]} the cards with the change applied
  */
 export function setItem(cards, position, replacementCard) {
-  throw new Error('Implement the setItem function');
+  cards[position] = replacementCard
+  return cards
 }
 
 /**
@@ -33,8 +34,10 @@ export function setItem(cards, position, replacementCard) {
  *
  * @returns {number[]} the cards with the newCard applied
  */
-export function insertItemAtTop(cards, newCard) {
-  throw new Error('Implement the insertItemAtTop function');
+export function insertItemAtTop(cards, newCard) { 
+  cards=cards.concat(newCard)
+  console.log(cards)
+  return cards;
 }
 
 /**
