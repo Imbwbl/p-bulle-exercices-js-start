52028bae21bb104d44f20b5528933174167574c7
bwbl <theophile2006@gmail.com>
2025-11-21 14:06:11 +0100

    finished bob

--- exercices/bob/bob.js
+++ exercices/bob/bob.js
@@ -3,6 +3,18 @@
 // convenience to get you started writing code faster.
 //
 
+const regex1 = /^[^a-z]*$/g;
+const regex2 = /.*[A-Z].*/g;
+
 export const hey = (message) => {
-  throw new Error('Remove this line and implement the function');
+  message = message.trim()
+  if (message.endsWith("?") && message.match(regex1) && message.match(regex2))
+    return "Calm down, I know what I'm doing!"
+  if (message.endsWith("?"))
+    return "Sure."
+  if (message.match(regex1) && message.match(regex2))
+    return "Whoa, chill out!"
+  if (message.match(/^\s*$/g))
+    return "Fine. Be that way!"
+  return "Whatever."
 };
