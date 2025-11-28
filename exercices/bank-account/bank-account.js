270a85943dd4f3e8c409892a484b9883d32f7fd0
bwbl <bwbl@ik.me>
2025-11-28 13:33:23 +0100

    finished bank-account

--- exercices/bank-account/bank-account.js
+++ exercices/bank-account/bank-account.js
@@ -5,40 +5,44 @@
 
 export class BankAccount {
   is_open = false
-  balance = 0;
+  #balance = 0;
   constructor() {
-    this.balance = 0;
+    this.#balance = 0;
   }
 
   open() {
     if (this.is_open == false)
       this.is_open = true
+    else
+      throw new ValueError()
   }
 
   close() {
-    if (this.is_open == true)
+    if (this.is_open == true) {
+      this.#balance = 0;
       this.is_open = false
+    }
     else
       throw new ValueError()
   }
 
   deposit(amount) {
-    if (this.is_open === true)
-      this.balance += amount
+    if (this.is_open === true && amount > 0)
+      this.#balance += amount
     else
       throw new ValueError()
   }
 
   withdraw(amount) {
-    if (this.is_open === true)
-      this.balance -= amount
+    if (this.is_open === true && this.#balance >= amount && amount > 0)
+      this.#balance -= amount
     else
       throw new ValueError()
   }
 
   get balance() {
     if (this.is_open === true) 
-      return this.balance
+      return this.#balance
     else
       throw new ValueError()
   }
