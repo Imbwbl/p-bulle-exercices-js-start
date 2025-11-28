56e29aca4d570b2855ad729fdccb36afbdf3ad00
bwbl <bwbl@ik.me>
2025-11-28 18:08:26 +0100

    finished promises

--- exercices/promises/promises.js
+++ exercices/promises/promises.js
@@ -3,22 +3,111 @@
 // convenience to get you started writing code faster.
 //
 
-export const promisify = () => {
-  throw new Error('Remove this line and implement the function');
+export function promisify(func) {
+  return (...args) => new Promise((resolve, reject)=>{
+    func(...args, (err, result) => {
+      if (err) 
+        reject(err)
+      else
+        resolve(result)
+      }
+  )})
 };
 
-export const all = () => {
-  throw new Error('Remove this line and implement the function');
+export const all = (promises) => {
+  return new Promise((resolve, reject) => {
+    let list = []
+    let c = 0;
+    if (!promises){
+      resolve()
+      return
+    }
+    if (promises.length === 0) 
+      resolve([])
+    promises.forEach((p, i) => {
+      p.catch(
+        (err) => {
+          reject(err)
+        }
+      ).then((data) => {
+        c++
+        list[i] = data
+        if (c === promises.length)
+          resolve(list)
+      }) 
+    });
+    
+  })
 };
 
-export const allSettled = () => {
-  throw new Error('Remove this line and implement the function');
+export const allSettled = (promises) => {
+  return new Promise((resolve, reject) => {
+    let list = []
+    let c = 0;
+    if (!promises){
+      resolve()
+      return
+    }
+    if (promises.length === 0) 
+      resolve([])
+    promises.forEach((p, i) => {
+      p.then((data) => {
+        c++
+        list[i] = data
+        if (c === promises.length)
+          resolve(list)
+      }).catch(
+        (err) => {
+          c++
+          list[i] = err
+          if (c === promises.length)
+            resolve(list)
+        }
+      ) 
+    });
+    
+  })
 };
 
-export const race = () => {
-  throw new Error('Remove this line and implement the function');
+export const race = (promises) => {
+  return new Promise((resolve, reject) => {
+    if (!promises){
+      resolve()
+      return
+    }
+    if (promises.length === 0) 
+      resolve([])
+    promises.forEach((p, i) => {
+      p.then((data) => {
+        resolve(data)
+      }).catch((e) => {
+        reject(e)
+      })
+    })
+  })
 };
 
-export const any = () => {
-  throw new Error('Remove this line and implement the function');
+export const any = (promises) => {
+  return new Promise((resolve, reject) => {
+    let list = []
+    let c = 0;
+    if (!promises){
+      resolve()
+      return
+    }
+    if (promises.length === 0) 
+      resolve([])
+    promises.forEach((p, i) => {
+      p.then((data) => {
+          resolve(data)
+      }).catch(
+        (err) => {
+          c++
+          list[i] = err
+          if (c === promises.length)
+            reject(list)
+        }
+      ) 
+    });
+  })
 };
