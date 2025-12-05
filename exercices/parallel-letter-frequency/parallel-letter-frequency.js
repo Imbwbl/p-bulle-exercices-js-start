7fd1a3ed34f6678d7653945dbde28e24feb88e08
Théophile Borboën <pi72mlj@eduvaud.ch>
2025-12-05 13:55:41 +0100

    WIP missing worker

--- exercices/parallel-letter-frequency/parallel-letter-frequency.js
+++ exercices/parallel-letter-frequency/parallel-letter-frequency.js
@@ -3,6 +3,22 @@
 // convenience to get you started writing code faster.
 //
 
+
+
 export const parallelLetterFrequency = async (texts) => {
-  throw new Error('Remove this line and implement the function');
+  let o = {}
+  let exclude = [" ", "!", "\\", "'", "(", ")", ",", "-", ".", ":", ";", "?", "\"", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "\t", "\n", "\r"]
+  texts.forEach(t => {
+    t.trim().toLowerCase().split("").forEach(c => {
+      if (!exclude.includes(c)) {
+        if (!o[c]) {
+          o[c] = 0
+        }
+        o[c] += 1;
+      }
+    })
+    
+  });
+  console.log(o);
+  return o
 };
